7338dabbd69cee5bf8f63b42a231b71c8e5fdee6
noahaustin01 <noah@austinlight.com>
2025-03-18 12:08:09 -0400

    Sudoku with Original

---
+++ src/main/java/com/mycompany/sudoku/AppOriginal.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.sudoku;
+
+/**
+ *
+ * @author noah
+ */
+public class AppOriginal {
+    public static void main(String[] args) throws Exception {
+        SudokuOriginal sudoku = new SudokuOriginal();
+    }
+}
